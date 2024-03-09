164e59e3ebb586de069d8c17bdd8271916ab288e
Kanmi Obasa <kanmi@konfirmed.com>
2024-03-09 11:57:29 -0800

    work work work work work

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-const Select = ({ id, required, children }) => {
+interface SelectProps {
+  id: string;
+  required: boolean;
+  children: ReactNode;
+}
+
+const Select: React.FC<SelectProps> = ({ id, required, children }) => {
   return (
     <select
+      title='Select Input'
       id={id}
       required={required}
       className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
