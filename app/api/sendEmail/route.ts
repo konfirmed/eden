a9bf66e447037869d62f97840fba62e005f51ea3
Caleb <feranmioyelowo@gmail.com>
2024-04-03 18:10:33 +0100

    minor changes

---
+++ app/api/sendEmail/route.ts
@@ -0,0 +1,41 @@
+import nodemailer from 'nodemailer';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+export async function OPTIONS(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Allow', 'POST');
+  res.status(200).end();
+}
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  console.log('Received form data:', req.body);
+
+  const formData = req.body; // Access the parsed form data directly
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to: process.env.EMAIL_TO,
+    subject: `Contact Form Submission: ${formData.subject}`,
+    text: `
+      Name: ${formData.name}
+      Email: ${formData.email}
+      Message: ${formData.message}
+    `
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: 'Error sending email' });
+  }
+}
